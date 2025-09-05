7b0f494d491cca228b2f29cab44e7b35c5f75988
samueln <salumas@gmail.com>
2025-09-04 22:06:39 -0300

    criando lista de usuario

---
+++ src/gerenciarUsuarios.js
@@ -0,0 +1,9 @@
+const usuarios = ['mateus', 'marcos', 'lucas', 'joão'];
+
+function retornaUsuarios() {
+    return usuarios;
+}
+
+function adicionarNovoUsuario(nome) {
+    usuarios.push(nome);
+}
